5e4e13b4bd22ce2538525b7e71069099b912d5c3
古田泰大 <yasuhiro2001@icloud.com>
2015-09-13 15:56:57 +0900

    firstcommit

---
+++ delegateTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  delegateTest
+//
+//  Created by 古田泰大 on 2015/09/13.
+//  Copyright (c) 2015年 古田泰大. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
